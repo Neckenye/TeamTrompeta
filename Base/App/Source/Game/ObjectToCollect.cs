f46610319ac15581867d4aa01900e4293b5b242c
Neckenye <danimartineznavarro16@gmail.com>
2025-05-20 20:05:25 +0200

    Squashed commit of the following:
    commit 203dd733e4d9b8d1219e450fadf621901c245644
    Author: Neckenye <danimartineznavarro16@gmail.com>
    Date:   Tue May 20 19:51:57 2025 +0200
    
        Starter Object To Collect

---
+++ Base/App/Source/Game/ObjectToCollect.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace TcGame
+{ 
+    public class ObjectToCollect : Actor
+    {
+        private Sprite sprite;
+        public ObjectToCollect()
+        {
+            sprite = new Sprite(new Texture("Data/Textures/Object/Coin.png"));
+            Layer = ELayer.Middle;
+            Position = new Vector2f(200f, 200f);
+        }
+
+
+    }
+}
