d2d1082bba0c627bc10c62a17d34b5e694b1532d
RealMikiMon <monereomiquel@gmail.com>
2025-05-21 13:48:59 +0200

    Moviment Final

--- Base/App/Source/Game/Player.cs
+++ Base/App/Source/Game/Player.cs
@@ -6,37 +6,30 @@ namespace TcGame
 {
     public class Player : StaticActor
     {
-        
+
 
         public Player()
         {
             Layer = ELayer.Front;
             Sprite = new Sprite(new Texture("Data/Textures/Player/Plane.png"));
-            Position = new Vector2f(Engine.Get.Window.Size.X / 2, Engine.Get.Window.Size.Y / 2);
+            Position = new Vector2f (Engine.Get.Window.Size.X/2, Engine.Get.Window.Size.Y/2);
             Speed = 200;
         }
 
-       
         public override void Update(float dt)
         {
-            base.Update(dt);
+            Vector2f movement = new Vector2f(0f, 0f);
 
-            if (Keyboard.IsKeyPressed(Keyboard.Key.A))
-            {
-                Forward = new Vector2f(-1, Forward.Y).Normal();
-            }
-            if (Keyboard.IsKeyPressed(Keyboard.Key.D))
-            {
-                Forward = new Vector2f(1, Forward.Y).Normal();
-            }
             if (Keyboard.IsKeyPressed(Keyboard.Key.W))
-            {
-                Forward = new Vector2f(Forward.X, -1).Normal();
-            }
+                movement.Y -= Speed * dt;
             if (Keyboard.IsKeyPressed(Keyboard.Key.S))
-            {
-                Forward = new Vector2f(Forward.X, 1).Normal();
-            }
+                movement.Y += Speed * dt;
+            if (Keyboard.IsKeyPressed(Keyboard.Key.A))
+                movement.X -= Speed * dt;
+            if (Keyboard.IsKeyPressed(Keyboard.Key.D))
+                movement.X += Speed * dt;
+
+            Sprite.Position += movement;
         }
     }
 }
