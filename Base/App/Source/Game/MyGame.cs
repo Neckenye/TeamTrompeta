1e0b8614079614fe522170ba05da8d746c62e81d
jgarciagra <jgarciagra@edu.tecnocampus.cat>
2025-05-21 10:01:50 +0200

    Revert "Merge branch 'main' into Eloy-Branch"
    This reverts commit 497275b6b7bd03cf7cb01c181b563366b90ae85c, reversing
    changes made to 2b41f2f3432aff7482e48a6b7a65165580777e54.

--- Base/App/Source/Game/MyGame.cs
+++ Base/App/Source/Game/MyGame.cs
@@ -1,138 +1,53 @@
-﻿using App.Source.Game;
-using SFML.Graphics;
+﻿using SFML.Graphics;
 using SFML.System;
 using SFML.Window;
 using System;
-using System.Collections.Generic;
-using System.Numerics;
+using System.Diagnostics.Contracts;
 
 namespace TcGame
 {
-    public class MyGame : Game
+  public class MyGame : Game
+  {
+        
+    public Hud hud { private set; get; }
+    public Background background { get;  private set;}
+    public GameOverScreen gameOverScreen { get; private set;}
+    private static MyGame instance;
+    public static MyGame Get
     {
-        public List<ObjectToCollect> objectList = new List<ObjectToCollect>();
-        public Front front { private set; get; }
-        public Hud hud { private set; get; }
-        public Background background { get; private set; }
+      get
+      {
+        if (instance == null)
+        {
+          instance = new MyGame();
+        }
 
-        private float timer;
-
-        private static MyGame instance;
+        return instance;
+      }
+    }
+    private MyGame()
+    {
+    }
+    public void Init()
+    {
+      gameOverScreen = Engine.Get.Scene.Create<GameOverScreen>();
 
       
-
-        //public static float timeLeft = 120f;
-        public static float timeLeft = 2;  //PER SABER SI VA
-
-
-        public static bool timeOver = false;
-
-    
-
-
-
-
-        public static MyGame Get
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    instance = new MyGame();
-                }
-
-                return instance;
-            }
-        }
-
-        private MyGame()
-        {
-        }
-
-        public void Init()
-        {
-            background = Engine.Get.Scene.Create<Background>();
-            hud = Engine.Get.Scene.Create<Hud>();
-            front = Engine.Get.Scene.Create<Front>();
-
-            timer = 0f;
-
-            ObjectToCollect first = Engine.Get.Scene.Create<ObjectToCollect>();
-            ObjectToCollect second = Engine.Get.Scene.Create<ObjectToCollect>();
-            ObjectToCollect third = Engine.Get.Scene.Create<ObjectToCollect>();
-            ObjectToCollect forth = Engine.Get.Scene.Create<ObjectToCollect>();
-
-            objectList.Add(first);
-            objectList.Add(second);
-            objectList.Add(third);
-            objectList.Add(forth);
-                      
-
-            Engine.Get.Scene.Create<TimeTxt>();
-        }
-
-        public void DeInit()
-        {
-
-        }
-
-        public void Update(float dt)
-        {
-            if (!timeOver)
-            {
-                timeLeft -= dt;
-
-                if (timeLeft <= 0f)
-                {
-                    timeLeft = 0f;
-                    timeOver = true;
-                }
-            }
-
-
-            hud.Update(dt);
-
-            timer += dt;
-
-            if (timer > 7)
-            {
-                objectList[0].Destroy();
-                objectList[1].Destroy();
-                objectList[2].Destroy();
-                objectList[3].Destroy();
-                objectList.RemoveAll(x => x != null);
-                timer = 0f;
-
-                ObjectToCollect first = Engine.Get.Scene.Create<ObjectToCollect>();
-                ObjectToCollect second = Engine.Get.Scene.Create<ObjectToCollect>();
-                ObjectToCollect third = Engine.Get.Scene.Create<ObjectToCollect>();
-                ObjectToCollect forth = Engine.Get.Scene.Create<ObjectToCollect>();
-
-                objectList.Add(first);
-                objectList.Add(second);
-                objectList.Add(third);
-                objectList.Add(forth);
-            }
-
-            for (int i = objectList.Count - 1; i >= 0; i--)
-            {
-                objectList[i].Update(dt);
-                if (front.GetGlobalBounds().Intersects(objectList[i].GetGlobalBounds()))
-                {
-                    objectList.RemoveAt(i);
-                }
-            }
-        }
-
-        private void DestroyAll<T>() where T : Actor
-        {
-            var actors = Engine.Get.Scene.GetAll<T>();
-            actors.ForEach(x => x.Destroy());
-        }
-
-
     }
+       //Hola
+    public void DeInit()
+    {
+    }
+    public void Update(float dt)
+    {
+            
+      
+    }
+    private void DestroyAll<T>() where T : Actor
+    {
+      var actors = Engine.Get.Scene.GetAll<T>();
+      actors.ForEach(x => x.Destroy());
+    }
+  }
 }
-
-
 
