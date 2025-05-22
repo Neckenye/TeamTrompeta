7c4c873f2542ae299a414a716e1189345734b37f
RealMikiMon <monereomiquel@gmail.com>
2025-05-22 11:28:57 +0200

    nada de nada

--- Base/App/Source/Game/ActorSpawner.cs
+++ Base/App/Source/Game/ActorSpawner.cs
@@ -14,9 +14,45 @@ namespace TcGame
     private float coolDown;
     public void Reset()
     {
+<<<<<<< Updated upstream
       Random r = new Random();
       float d = (float)r.NextDouble();
       coolDown = MaxTime * d + (1.0f - d) * MinTime;
+=======
+
+        public float MaxTime = 6.5f;
+        public Vector2f MinPosition;
+        public Vector2f MaxPosition;
+        private float coolDown = 0f;
+        public void Reset()
+        {
+            Random r = new Random();
+            float d = (float)r.NextDouble();
+            coolDown = MaxTime;
+        }
+        public override void Update(float dt)
+        {
+            base.Update(dt);
+            coolDown -= dt;
+            if (coolDown < 0.0f)
+            {                 
+                SpawnActor();
+                Reset();
+            }
+        }
+        public void SpawnActor()
+        {
+            Random r = new Random();
+            float d1 = (float)r.NextDouble();
+            float d2 = (float)r.NextDouble();
+
+            float X = MaxPosition.X * d1 + (1.0f - d1) * MinPosition.X;
+            float Y = MaxPosition.Y * d2 + (1.0f - d2) * MinPosition.Y;
+
+            Actor a = Engine.Get.Scene.Create<T>();
+            a.Position = new Vector2f(X, Y);
+        }
+>>>>>>> Stashed changes
     }
     public override void Update(float dt)
     {
---
+++ Base/App/Source/Game/PantallaNegraVision.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TcGame
+{
+    public class PantallaNegraVision : StaticActor
+    {
+        public float time = 7f;
+
+        private Sprite telonNegro;
+
+
+        public PantallaNegraVision()
+        {
+            Layer = ELayer.Middle;
+
+            Texture texture = new Texture("Data/Textures/Hud/niga2.png");
+
+            telonNegro = new Sprite(texture);
+            telonNegro.Scale = telonNegro.Scale * 0.95f;
+
+        }
+
+        public override void Update(float dt)
+        {
+            base.Update(dt);
+
+            time -= dt;
+        }
+        public override void Draw(RenderTarget target, RenderStates states)
+        {
+
+            
+            //Si el tiempo es 0 resetealo, y si es menos de 5 se muestra la pantalla negra
+             
+            if (time <= 0)
+            {
+                time = 6.5f;
+            }
+            else if (time <= 5)
+            {
+                target.Draw(telonNegro);
+            }
+        }
+    }
+}
