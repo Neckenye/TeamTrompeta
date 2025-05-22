eafb1ee0c01c2575d159fa2986a1b5372deba702
Neckenye <danimartineznavarro16@gmail.com>
2025-05-22 03:07:08 +0200

    Puntuacion final appears

--- Base/App/Source/Game/Hud.cs
+++ Base/App/Source/Game/Hud.cs
@@ -7,15 +7,19 @@ namespace TcGame
 {   
     public class Hud : Actor
     {
-        public float time = 120f; //Este tiempo cuando se acabe será game over
+        public float time = 20f; //Este tiempo cuando se acabe será game over
         public int pointsColected;
+        public int finalPoints;
+
+        private bool coinColect = true;
 
         private Text noTimeTxt; // Este es el texto de GAME OVER
         private Text txt;
+        private Text puntFinal; // Este es el texto de PUNTUACION FINAL
         private Text timer;
+
         private Sprite cuadradoGigante; // Esta es la imagen de fondo del Game over
         
-
         public Hud()
         {
             Layer = ELayer.Hud;
@@ -26,10 +30,15 @@ namespace TcGame
 
             txt = new Text("", f);
             timer = new Text("", f);
+            puntFinal = new Text("", f);
             noTimeTxt = new Text("GAME OVER", f);
 
             noTimeTxt.Origin = new Vector2f(GetLocalBounds().Width, GetLocalBounds().Height) / 2.0f;
             noTimeTxt.Scale = noTimeTxt.Scale * 3;
+
+            puntFinal.Origin = new Vector2f(GetLocalBounds().Width, GetLocalBounds().Height) / 2.0f;
+            puntFinal.Position = new Vector2f(Engine.Get.Window.Size.X / 2 - 120, Engine.Get.Window.Size.Y / 2 + 150);
+
 
             txt.Position = new Vector2f(10, 10);
             timer.Position = new Vector2f(Engine.Get.Window.Size.X - 50, 10);
@@ -41,6 +50,7 @@ namespace TcGame
             noTimeTxt.FillColor = Color.Red;
             txt.FillColor = Color.White;
             timer.FillColor = Color.White;
+            puntFinal.FillColor = Color.White;
 
         }
 
@@ -50,6 +60,11 @@ namespace TcGame
             SetText();
 
             time -= dt;
+
+            if (coinColect)
+            {
+                finalPoints = pointsColected;
+            }
         }
 
         public void SetText()
@@ -70,8 +85,13 @@ namespace TcGame
 
             if (time <= 0) // Cuando se acabe el tiempo se muestra el GAME OVER con el gris de fondo
             {
+                coinColect = false;
+                puntFinal.DisplayedString = ($"Points Colected: {finalPoints}");
+
                 target.Draw(cuadradoGigante);
                 target.Draw(noTimeTxt);
+                target.Draw(puntFinal);
+
             }
         }
     }
